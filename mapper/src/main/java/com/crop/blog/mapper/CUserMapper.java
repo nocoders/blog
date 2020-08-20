2b2302a086b32ab720f8408aa2c55db88dd5c94c
linmeng <1812323571@qq.com>
2020-08-20 16:03:02 +0800

    重新生成实体类， 移除mybatis-plus,使用mybatis-generator 删除service模块

---
+++ mapper/src/main/java/com/crop/blog/mapper/CUserMapper.java
@@ -0,0 +1,30 @@
+package com.crop.blog.mapper;
+
+import com.crop.blog.model.CUser;
+import com.crop.blog.model.CUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CUserMapper {
+    long countByExample(CUserExample example);
+
+    int deleteByExample(CUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CUser record);
+
+    int insertSelective(CUser record);
+
+    List<CUser> selectByExample(CUserExample example);
+
+    CUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") CUser record, @Param("example") CUserExample example);
+
+    int updateByExample(@Param("record") CUser record, @Param("example") CUserExample example);
+
+    int updateByPrimaryKeySelective(CUser record);
+
+    int updateByPrimaryKey(CUser record);
+}
---
+++ mapper/src/main/java/com/crop/blog/model/CUser.java
@@ -0,0 +1,116 @@
+package com.crop.blog.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+
+public class CUser implements Serializable {
+    @ApiModelProperty(value = "主键")
+    private Long id;
+
+    @ApiModelProperty(value = "用户名称")
+    private String userName;
+
+    @ApiModelProperty(value = "用户密码")
+    private String password;
+
+    @ApiModelProperty(value = "用户手机号码")
+    private String telephone;
+
+    @ApiModelProperty(value = "用户邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "用户昵称")
+    private String nickName;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", userName=").append(userName);
+        sb.append(", password=").append(password);
+        sb.append(", telephone=").append(telephone);
+        sb.append(", email=").append(email);
+        sb.append(", nickName=").append(nickName);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
