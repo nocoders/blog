5f03f87e0f7db848c9bbe21d358318dead74c25f
linmeng <1812323571@qq.com>
2020-08-22 20:25:59 +0800

    忘记提交的代码

---
+++ mapper/src/main/java/com/crop/mapper/mapper/CUserMapper.java
@@ -0,0 +1,32 @@
+package com.crop.mapper.mapper;
+
+import com.crop.mapper.model.CUser;
+import com.crop.mapper.model.CUserExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+@Mapper
+public interface CUserMapper {
+    long countByExample(CUserExample example);
+
+    int deleteByExample(CUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(CUser record);
+
+    int insertSelective(CUser record);
+
+    List<CUser> selectByExample(CUserExample example);
+
+    CUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") CUser record, @Param("example") CUserExample example);
+
+    int updateByExample(@Param("record") CUser record, @Param("example") CUserExample example);
+
+    int updateByPrimaryKeySelective(CUser record);
+
+    int updateByPrimaryKey(CUser record);
+}
---
+++ mapper/src/main/resources/db/dbInit.sql
@@ -0,0 +1,24 @@
+drop database if exists blog;
+
+create database blog default charset utf8 collate utf8_general_ci;
+
+use blog;
+
+drop table if exists c_user;
+
+/*==============================================================*/
+/* Table: c_user                                                */
+/*==============================================================*/
+create table c_user
+(
+   id                   bigint not null auto_increment,
+   user_name            varchar(32) not null default '' comment '用户名称',
+   password             varchar(64) not null default '' comment '密码',
+   telephone            varchar(11) default '' comment '手机号码',
+   email                varchar(32) default '' comment '邮箱',
+   nick_name            varchar(64) not null default '' comment '昵称',
+   icon_url             varchar(128) default '' comment '头像url',
+   create_time          timestamp default CURRENT_TIMESTAMP comment '创建时间',
+   update_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '修改时间',
+   primary key (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
